024b19c7f5aba02d194d308e092e7bbe55b63cdb
machida4 <machida@kmc.gr.jp>
2025-04-27 01:00:43 +0900

    first commit

---
+++ haskell/recursive.hs
@@ -0,0 +1,40 @@
+replicate' :: Int -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x : replicate' (n - 1) x
+
+take' :: Int -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x : xs) = x : take' (n - 1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x : xs) = reverse' xs ++ [x]
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x : xs) (y : ys) = (x, y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x : xs)
+  | a == x = True
+  | otherwise = a `elem'` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x : xs) =
+  let smallerOrEqual = [a | a <- xs, a <= x]
+      larger = [a | a <- xs, a > x]
+   in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
+main = do
+  print $ replicate' 10 "ABS"
+  print $ take' 20 ['A' .. 'Z']
+  print $ reverse' "HELLO, WORLD!"
+  print $ zip' [1, 2, 3] [7, 8]
+  print $ 3 `elem'` [1 .. 10]
+  print $ quicksort [10, 2, 3, 4, 2, 9, 4, 8, 3, 13, 5]
